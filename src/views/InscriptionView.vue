61a3b1a10200403b3122749e0f2f110a6354005a
unknown <khalilkrayem@hotmail.com>
2023-11-18 18:49:02 -0500

    fixes

--- src/views/InscriptionView.vue
+++ src/views/InscriptionView.vue
@@ -1,6 +1,5 @@
 <template>
-    <Toast />
-    <h1 class="text-3xl">Modifier les propriétés</h1>
+    <h1 class="text-3xl">Inscrire son équipe</h1>
     <div class="grid gap-6 mt-4 font-semibold">
         <InputWord label="Le nom de ton équipe" />
         <h1 v-if="teamId">Voici votre identifiant : {{ teamId }}</h1>
@@ -10,19 +9,15 @@
 
 <script setup lang="ts">
 import Button from "primevue/button";
-import Toast from "primevue/toast";
 
-import { useToast } from 'primevue/usetoast';
 import InputWord from "@/components/InputWord.vue";
 import { ref, type Ref } from "vue";
 
-const toast = useToast();
 
 let teamId: Ref<string | null> = ref(null);
 
 const showTopLeft = () => {
     teamId.value = generateRandomString();
-    toast.add({ severity: 'info', summary: 'Propriétés modifiées avec succès', detail: 'Les changements seront appliqués dès la prochaine partie.', life: 3000 });
 };
 
 const generateRandomString = () => {
